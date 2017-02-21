c86aeba65454a86b18c09a25c61fbb5d1e2734a3
ipjohnson <ipjohnson123@yahoo.com>
2017-02-21 06:14:52 -0700

    adding tests

--- tests/EasyRpc.Tests/Middleware/OrderedParameterMethodInvokeBuilderTests.cs
+++ tests/EasyRpc.Tests/Middleware/OrderedParameterMethodInvokeBuilderTests.cs
@@ -7,6 +7,8 @@ using EasyRpc.AspNetCore.Messages;
 using EasyRpc.AspNetCore.Middleware;
 using EasyRPC.AspNetCore.Tests.Classes;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
 using SimpleFixture.NSubstitute;
 using SimpleFixture.xUnit;
 using Xunit;
@@ -16,6 +18,7 @@ namespace EasyRpc.Tests.Middleware
     [SubFixtureInitialize]
     public class OrderedParameterMethodInvokeBuilderTests
     {
+        #region No return value
         public class TestClass
         {
             public void Execute(int a)
@@ -30,7 +33,8 @@ namespace EasyRpc.Tests.Middleware
 
             public int A { get; set; }
         }
-        
+
+
         [Theory]
         [AutoData]
         public void OrderedParameterMethodInvokeBuilder_ReturnVoid(HttpContext context)
@@ -69,7 +73,7 @@ namespace EasyRpc.Tests.Middleware
             Assert.NotNull(response);
             Assert.Equal(5, response.Result);
         }
-        
+
         [Theory]
         [AutoData]
         public void OrderedParameterMethodInvokeBuilder_ReturnTaskInt(HttpContext context)
@@ -89,7 +93,7 @@ namespace EasyRpc.Tests.Middleware
             Assert.NotNull(response);
             Assert.Equal(5, response.Result);
         }
-        
+
         [Theory]
         [AutoData]
         public void OrderedParameterMethodInvokeBuilder_ReturnTask(HttpContext context)
@@ -108,5 +112,136 @@ namespace EasyRpc.Tests.Middleware
 
             Assert.Equal(5, testClass.A);
         }
+        #endregion
+
+        #region return value
+
+        public interface IAdder
+        {
+            int Add(int a, int b);
+        }
+
+        public class Calculator
+        {
+            public int Add(int a, int b)
+            {
+                return a + b;
+            }
+
+            public async Task<int> TaskAdd(int a, int b)
+            {
+                return a + b;
+            }
+
+            public int AddFromService([FromServices] IAdder adder, int a, int b)
+            {
+                return adder.Add(a, b);
+            }
+
+            public async Task<int> TaskAddFromService([FromServices] IAdder adder, int a, int b)
+            {
+                return adder.Add(a, b);
+            }
+
+        }
+
+
+        [Theory]
+        [AutoData]
+        public void OrderedParameterMethodInvokeBuilder_Return_Int(HttpContext context)
+        {
+            var invoker = new OrderedParameterMethodInvokeBuilder();
+
+            var method = typeof(Calculator).GetRuntimeMethod("Add", new[] { typeof(int), typeof(int) });
+
+            var parameterMethod = invoker.BuildInvokeMethodOrderedParameters(method);
+
+            var calculator = new Calculator();
+
+            var returnValueTask = parameterMethod("2.0", "id", calculator, new object[] { 5, 10 }, context);
+
+            returnValueTask.Wait();
+
+            var responseMessage = returnValueTask.Result as ResponseMessage<int>;
+
+            Assert.NotNull(responseMessage);
+            Assert.Equal(15, responseMessage.Result);
+        }
+
+        [Theory]
+        [AutoData]
+        public void OrderedParameterMethodInvokeBuilder_Return_Int_TaskAdd(HttpContext context)
+        {
+            var invoker = new OrderedParameterMethodInvokeBuilder();
+
+            var method = typeof(Calculator).GetRuntimeMethod("TaskAdd", new[] { typeof(int), typeof(int) });
+
+            var parameterMethod = invoker.BuildInvokeMethodOrderedParameters(method);
+
+            var calculator = new Calculator();
+
+            var returnValueTask = parameterMethod("2.0", "id", calculator, new object[] { 5, 10 }, context);
+
+            returnValueTask.Wait();
+
+            var responseMessage = returnValueTask.Result as ResponseMessage<int>;
+
+            Assert.NotNull(responseMessage);
+            Assert.Equal(15, responseMessage.Result);
+        }
+        
+        [Theory]
+        [AutoData]
+        public void OrderedParameterMethodInvokeBuilder_Return_Int_FromService(HttpContext context, IAdder adder)
+        {
+            context.RequestServices.GetService(typeof(IAdder)).Returns(adder);
+
+            adder.Add(5, 10).Returns(15);
+
+            var invoker = new OrderedParameterMethodInvokeBuilder();
+
+            var method = typeof(Calculator).GetRuntimeMethod("AddFromService", new[] { typeof(IAdder), typeof(int), typeof(int) });
+
+            var parameterMethod = invoker.BuildInvokeMethodOrderedParameters(method);
+            
+            var calculator = new Calculator();
+
+            var returnValueTask = parameterMethod("2.0", "id", calculator, new object[] { 5, 10 }, context);
+
+            returnValueTask.Wait();
+
+            var responseMessage = returnValueTask.Result as ResponseMessage<int>;
+
+            Assert.NotNull(responseMessage);
+            Assert.Equal(15, responseMessage.Result);
+        }
+
+        [Theory]
+        [AutoData]
+        public void OrderedParameterMethodInvokeBuilder_Return_TaskInt_FromService(HttpContext context, IAdder adder)
+        {
+            context.RequestServices.GetService(typeof(IAdder)).Returns(adder);
+
+            adder.Add(5, 10).Returns(15);
+
+            var invoker = new OrderedParameterMethodInvokeBuilder();
+
+            var method = typeof(Calculator).GetRuntimeMethod("TaskAddFromService", new[] { typeof(IAdder), typeof(int), typeof(int) });
+
+            var parameterMethod = invoker.BuildInvokeMethodOrderedParameters(method);
+
+            var calculator = new Calculator();
+
+            var returnValueTask = parameterMethod("2.0", "id", calculator, new object[] { 5, 10 }, context);
+
+            returnValueTask.Wait();
+
+            var responseMessage = returnValueTask.Result as ResponseMessage<int>;
+
+            Assert.NotNull(responseMessage);
+            Assert.Equal(15, responseMessage.Result);
+        }
+
+        #endregion
     }
 }
