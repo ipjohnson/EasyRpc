30096f94cd12eac788c289081b3fd43f3d2dd379
ipjohnson <ipjohnson123@yahoo.com>
2017-02-24 17:40:06 -0700

    adding default rpc client provider

---
+++ src/EasyRpc.DynamicClient/DefaultRpcClientProvider.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EasyRpc.DynamicClient
+{
+    public class DefaultRpcClientProvider : IRpcHttpClientProvider, IDisposable
+    {
+        private readonly bool _callByNamedParameter;
+        private readonly int _timeout;
+        private HttpClient _client;
+
+        public DefaultRpcClientProvider(string url, bool callByNamedParameter = false, int timeout = 30)
+        {
+            _callByNamedParameter = callByNamedParameter;
+            _timeout = timeout;
+            _client = new HttpClient { BaseAddress = new Uri(url) };
+        }
+
+        public IRpcHttpClient GetHttpClient(Type type)
+        {
+            return new RpcHttpClient(_client, _callByNamedParameter, _timeout);
+        }
+
+        public void ReturnHttpClient(Type type, IRpcHttpClient client)
+        {
+
+        }
+
+        public void Dispose()
+        {
+            var client = Interlocked.Exchange(ref _client, null);
+
+            client?.Dispose();
+        }
+    }
+}
