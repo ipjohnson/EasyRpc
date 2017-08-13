21b7d0f77d8d1d193a268d199c9dfb3292cf23bb
ipjohnson <ipjohnson123@yahoo.com>
2017-08-13 15:09:14 -0600

    changes to make it easier to use

--- src/EasyRpc.DynamicClient.Grace/LanguageExtensions.cs
+++ src/EasyRpc.DynamicClient.Grace/LanguageExtensions.cs
@@ -23,35 +23,68 @@ namespace EasyRpc.DynamicClient.Grace
         IProxyConfiguration ProxyNamespaceContaining<T>(string url);
     }
 
+    public class ProxyNamscapeConfig
+    {
+        public string Url { get; set; }
+
+        public bool CallByName { get; set; }
+
+        public bool UseDataContext { get; set; }
+
+        public Action<JsonSerializer> SerializerInit { get; set; }
+
+        public string[] Namespaces { get; set; }
+    }
+
     public static class LanguageExtensions
     {
-        public static void SetupProxys(this IInjectionScope scope, Action<IProxyCollectionConfiguration> configuration)
-        {
-            scope.Configure(c =>
+        public static void ProxyNamescape(this IExportRegistrationBlock block, ProxyNamscapeConfig config)
+        {                // I mark everything as -1 priority so that if another version is registered it overrides these defaults
+            block.Export<ProxyGenerator.ProxyGenerator>().As<IProxyGenerator>().WithPriority(-1).Lifestyle.Singleton();
+            block.Export<DefaultNamingConventionService>().As<INamingConventionService>().WithPriority(-1);
+            block.ExportInstance(() => new DefaultRpcClientProvider(config.Url ?? "ReplaceMe")).As<IRpcHttpClientProvider>().WithPriority(-1);
+            block.Export<JsonMethodObjectWriter>().As<IJsonMethodObjectWriter>().Lifestyle.Singleton().WithPriority(-1);
+            block.Export<RpcProxyService>().As<IRpcProxyService>().WithPriority(-1).Lifestyle.SingletonPerScope();
+            
+            var serializerRegistration = 
+                block.Export<JsonSerializer>().WithPriority(-1).Lifestyle.Singleton();
+
+            if (config.SerializerInit != null)
             {
-                if (!c.OwningScope.CanLocate(typeof(IProxyGenerator)))
-                {
-                    
-                }
-            });
+                serializerRegistration.Apply(config.SerializerInit);
+            }
+
+            if (config.UseDataContext)
+            {
+                block.Export<DataContextHeaderProcessor>().As<IRpcContextHeader>().As<IHeaderProcessor>().WithPriority(-1).Lifestyle.SingletonPerScope();
+            }
+
+            block.AddMissingExportStrategyProvider(new ProxyStrategyProvider(config.CallByName, config.Namespaces));
         }
 
-
-        public static void ProxyNamespace(this IInjectionScope scope, string url, bool callByName, params string[] parameters)
+        public static void ProxyNamespace(this IExportRegistrationBlock block, string url = null,
+            bool callByName = true, bool useDataContext = false, params string[] namespaces)
         {
-            scope.Configure(c =>
+            block.ProxyNamescape(new ProxyNamscapeConfig
             {
-                // I mark everything as -1 priority so that if another version is registered it overrides these defaults
-                c.Export<ProxyGenerator.ProxyGenerator>().As<IProxyGenerator>().WithPriority(-1).Lifestyle.Singleton();
-                c.Export<DefaultNamingConventionService>().As<INamingConventionService>().WithPriority(-1);
-                c.ExportInstance(() => new DefaultRpcClientProvider(url)).As<IRpcHttpClientProvider>().WithPriority(-1);
-                c.Export<DataContextHeaderProcessor>().As<IRpcContextHeader>().As<IHeaderProcessor>().WithPriority(-1).Lifestyle.SingletonPerScope();
-                c.Export<JsonMethodObjectWriter>().As<IJsonMethodObjectWriter>().Lifestyle.Singleton().WithPriority(-1);
-                c.Export<RpcProxyService>().As<IRpcProxyService>().WithPriority(-1).Lifestyle.SingletonPerScope();
-                c.Export<JsonSerializer>().Lifestyle.Singleton();
-
-                c.AddMissingExportStrategyProvider(new ProxyStrategyProvider(callByName,parameters));
+                Url = url,
+                Namespaces = namespaces,
+                CallByName = callByName,
+                UseDataContext = useDataContext,
+                SerializerInit = null
             });
+        }
+        
+        public static void ProxyNamespace(this IInjectionScope scope, string url = null, bool callByName = true, bool useDataContext = false, params string[] namespaces)
+        {
+            scope.Configure(c => c.ProxyNamescape(new ProxyNamscapeConfig
+            {
+                Url = url,
+                Namespaces = namespaces,
+                CallByName = callByName,
+                UseDataContext = useDataContext,
+                SerializerInit = null
+            }));
         }
     }
 }
---
+++ tests/EasyRpc.Tests/DynamicClient/DataContextHeaderProcessorTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyRpc.Tests.DynamicClient
+{
+    public class DataContextHeaderProcessorTests
+    {
+
+    }
+}
