bd5a54eef32c56ecb9af94976c33ad67075e1c3f
Ian Johnson <ipjohnson123@yahoo.com>
2020-04-30 20:50:04 -0400

    adding HEAD and OPTIONS method attributes

---
+++ src/EasyRpc.Abstractions/Path/OptionsMethodAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace EasyRpc.Abstractions.Path
+{
+    public class OptionsMethodAttribute : Attribute, IPathAttribute
+    {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="path"></param>
+        public OptionsMethodAttribute(string path = null)
+        {
+            Path = path;
+        }
+
+        /// <inheritdoc />
+        public string Method => "OPTIONS";
+
+        /// <inheritdoc />
+        public string Path { get; }
+
+        /// <summary>
+        /// HTTP success status code
+        /// </summary>
+        public HttpStatusCode Success { get; set; } = HttpStatusCode.OK;
+
+        /// <inheritdoc />
+        int IPathAttribute.SuccessCodeValue => (int)Success;
+
+        /// <inheritdoc />
+        public bool HasBody => false;
+    }
+}
