283a9cc247205749c894a50fd967caa01c17df02
Ian Johnson <ipjohnson123@yahoo.com>
2020-08-03 20:37:03 -0400

    adding example apps

---
+++ examples/EasyRpc.Examples.ConsoleClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using EasyRpc.DynamicClient.Grace;
+using EasyRpc.Examples.SharedDefinitions;
+using Grace.DependencyInjection;
+
+namespace EasyRpc.Examples.ConsoleClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var container = new DependencyInjectionContainer();
+            
+            container.Configure(c => c.ProxyNamespace("https://localhost:44317/", namespaces: "EasyRpc.Examples.SharedDefinitions"));
+
+            var mathService = container.Locate<IMathService>();
+
+            var result = await mathService.Add(5, 10);
+        }
+    }
+}
---
+++ examples/EasyRpc.Examples.Service/Services/MathService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EasyRpc.Examples.SharedDefinitions;
+
+namespace EasyRpc.Examples.Service.Services
+{
+    public class MathService : IMathService
+    {
+        /// <inheritdoc />
+        public async Task<IntResult> Add(int a, int b)
+        {
+            return new IntResult { result = a + b };
+        }
+    }
+}
