cd00a24cc48486f76d986eaead3fa6dabb54b302
ipjohnson <ipjohnson123@yahoo.com>
2020-09-14 20:46:52 +0000

    CI Updates

--- src/EasyRpc.AspNetCore/Configuration/Delegates.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace EasyRpc.AspNetCore.Configuration
-{
-    public delegate bool TypeWrapSelector(Type typeToWrap);
-}
