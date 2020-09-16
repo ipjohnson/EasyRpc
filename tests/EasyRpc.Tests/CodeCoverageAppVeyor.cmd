537fa8a60fd57ec2c8a73b2b29f0e16886eaf0de
Ian Johnson <ipjohnson123@yahoo.com>
2020-09-15 21:12:19 -0400

    upgrading coveralls.net package

--- tests/EasyRpc.Tests/CodeCoverageAppVeyor.cmd
+++ tests/EasyRpc.Tests/CodeCoverageAppVeyor.cmd
@@ -2,7 +2,7 @@
 
 SET dotnet="C:/Program Files/dotnet/dotnet.exe"  
 SET opencover=..\..\tools\OpenCover.4.7.922\tools\OpenCover.Console.exe  
-SET coveralls=..\..\tools\coveralls.net.1.0.0\tools\csmacnz.Coveralls.exe
+SET coveralls=..\..\tools\coveralls.net.2.0.0-beta0002\tools\csmacnz.Coveralls.exe
 
 SET targetargs="test"  
 SET filter="+[*]EasyRpc.* -[EasyRpc.Tests]* -[xunit.*]* "  
@@ -10,7 +10,7 @@ SET coveragefile=Coverage.xml
 SET coveragedir=Coverage
 
 nuget install OpenCover -Version 4.7.922 -OutputDirectory ..\..\tools
-nuget install coveralls.net -Version 1.0.0 -OutputDirectory ..\..\tools
+nuget install coveralls.net -Version 2.0.0-beta0002 -OutputDirectory ..\..\tools
 
 REM Run code coverage analysis  
 %opencover% -oldStyle -returntargetcode -register:user -target:%dotnet% -output:%coveragefile% -targetargs:%targetargs% -filter:%filter% -skipautoprops -hideskipped:All
