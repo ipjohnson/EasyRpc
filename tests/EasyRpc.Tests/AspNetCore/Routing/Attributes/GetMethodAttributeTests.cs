bfe4b85a1cb0d5cd7294e1c1b711d06a5d01154a
Ian Johnson <ipjohnson123@yahoo.com>
2020-09-17 18:46:13 -0400

    stubbing out test class

---
+++ tests/EasyRpc.Tests/AspNetCore/Routing/Attributes/GetMethodAttributeTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using EasyRpc.AspNetCore;
+using Xunit;
+
+namespace EasyRpc.Tests.AspNetCore.Routing.Attributes
+{
+    public class GetMethodAttributeTests : BaseRequestTest
+    {
+        #region Service
+
+        public class Service
+        {
+
+        }
+
+        #endregion
+
+        #region Tests
+
+        [Fact]
+        public async Task GetMethodAttributeTests_Test()
+        {
+
+        }
+
+        #endregion
+
+        #region registration
+
+        protected override void ApiRegistration(IApiConfiguration api)
+        {
+
+        }
+
+        #endregion
+    }
+}
