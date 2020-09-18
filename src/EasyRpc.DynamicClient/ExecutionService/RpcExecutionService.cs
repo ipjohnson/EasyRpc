123f447ff3649dcc3d6c02a8464ac515f9c01b14
Ian Johnson <ipjohnson123@yahoo.com>
2020-09-18 11:01:53 -0400

    removing unused code

--- src/EasyRpc.DynamicClient/ExecutionService/RpcExecutionService.cs
+++ src/EasyRpc.DynamicClient/ExecutionService/RpcExecutionService.cs
@@ -11,50 +11,6 @@ namespace EasyRpc.DynamicClient.ExecutionService
 {
     public class RpcExecutionService : IRpcExecutionService
     {
-        public async Task<T> ExecuteMethod<T>(RpcExecuteInformation executeInformation, 
-            string path, 
-            object bodyParameters,
-            CancellationToken? cancellationToken)
-        {
-            var request = new HttpRequestMessage(executeInformation.Method, path);
-
-            if (bodyParameters != null)
-            {
-                await executeInformation.Serializer.SerializeToRequest(bodyParameters, request, executeInformation.CompressBody);
-            }
-
-            var client = executeInformation.ClientProvider.ProvideClient();
-
-            var response = await client.SendAsync(request, cancellationToken ?? CancellationToken.None);
-
-            if (response.IsSuccessStatusCode)
-            {
-                if (response.StatusCode == HttpStatusCode.NoContent)
-                {
-                    return default;
-                }
-
-                return await executeInformation.Serializer.DeserializeFromResponse<T>(response);
-            }
-
-            if (response.StatusCode == HttpStatusCode.NotFound)
-            {
-                return default;
-            }
-
-            if (response.StatusCode == HttpStatusCode.Unauthorized)
-            {
-                throw new UnauthorizedAccessException("User is not currently authorized");
-            }
-
-            if (response.StatusCode == HttpStatusCode.Forbidden)
-            {
-                throw new UnauthorizedAccessException("User is forbidden from executing method");
-            }
-
-            throw new Exception("Need to handle error");
-        }
-
         /// <inheritdoc />
         public async Task<T> ExecuteMethodWithValue<T>(RpcExecuteInformation executeInformation, string path, object bodyParameters,
             CancellationToken? cancellationToken)
